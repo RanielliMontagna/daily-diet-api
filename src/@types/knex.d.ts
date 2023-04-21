4f57ed3f96fa3fa16c579133a50e7c740f1645b5
RanielliMontagna <raniellimontagna@hotmail.com>
2023-04-21 12:43:54 -0300

    feat: :sparkles: implementa migrations

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -2,5 +2,20 @@
 import { Knex } from 'knex'
 
 declare module 'knex/types/tables' {
-  export interface Tables {}
+  export interface Tables {
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      isOnTheDiet: boolean
+      created_at: string
+    }
+    users: {
+      id: string
+      name: string
+      email: string
+      session_id?: string
+    }
+  }
 }
